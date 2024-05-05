aed28daf4824ce717ae99b6cfb0358c24302074c
Altynbekow <152465429+Altynbekow@users.noreply.github.com>
2024-05-05 09:23:07 +0600

    salamat

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+
 import Layout from './Layout/Layout'
+import Hero from "./components/hero/Hero.jsx";
+import Test from "./components/test/Test.jsx";
 
 
 const App = () => {
   return (
     <div>
+      <Hero/>
       <Layout/>
+      <Test/>
     </div>
   )
 }
---
+++ src/components/test/Test.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://6636002d415f4e1a5e25f5e0.mockapi.io/books');
+        setBooks(response.data);
+
+      } catch (error) {
+        console.log('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Список книг</h1>
+      <ul>
+        {books.map(book => (
+          <li key={book.id}>
+            <strong>{book.name}</strong> - {book.author}
+            <br/>
+            <p>{book.price}</p>
+            <img src={book.avatar} alt=""/>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
